8c9685bf8dc1f8372a92aa0f782a6e00d44819c8
yjiang <907292671@qq.com>
2022-05-05 13:40:48 +0800

    Graph heap

--- src/class16_graph/Dikjtra.java
+++ src/class16_graph/Dikjtra.java
@@ -37,6 +37,16 @@ public class Dikjtra {
         return node;
     }
 
+    public static class Record{
+        public Node node;
+        public Integer distance;
+
+        public Record(Node node, Integer distance) {
+            this.node = node;
+            this.distance = distance;
+        }
+    }
+
     public static class DikHeap{
         private Node[] nodes = null;
         private Map<Node, Integer> heapIndexMap;
@@ -45,17 +55,64 @@ public class Dikjtra {
 
         public DikHeap(int size) {
             nodes = new Node[size];
+            this.heapSize = 0;
             heapIndexMap = new HashMap<>();
             distanceMap = new HashMap<>();
         }
 
-//        public void insert(Node node, int distance){
-//            // never added
-//            if(heapIndexMap.containsKey(node)){
-//                nodes[heapSize] = node;
-//                heapIndexMap.put(node, heapSize);
-//                distanceMap.put(node, )
-//            }
-//        }
+        public void add(Node node, int distance){
+            // never added
+            if(heapIndexMap.containsKey(node)){
+                nodes[heapSize] = node;
+                heapIndexMap.put(node, heapSize);
+                distanceMap.put(node, distance);
+                heapSize++;
+                insert(heapSize-1);
+
+            }
+
+            if(distanceMap.containsKey(node)){
+                distanceMap.put(node, Math.min(distanceMap.get(node), distance));
+                insert(heapIndexMap.get(node));
+            }
+        }
+
+        public Record pop(){
+            Record record = new Record(nodes[0], distanceMap.get(nodes[0]));
+            distanceMap.remove(nodes[0]);
+            swap(0, --heapSize);
+            heapify(0);
+            return record;
+        }
+
+        public void insert(int index){
+            while(distanceMap.get(nodes[index]) < distanceMap.get(nodes[(index-1)/2])){
+                swap(index, (index-1)/2);
+                index = (index-1)/2;
+            }
+        }
+
+        public void heapify(int index){
+            int leftIndex = 2*index + 1;
+            while(leftIndex < heapSize){
+                int minIndex = leftIndex+1>=heapSize || distanceMap.get(nodes[leftIndex]) < distanceMap.get(nodes[leftIndex+1]) ? leftIndex : leftIndex+1;
+                if( distanceMap.get(minIndex) >= distanceMap.get(index)){
+                    break;
+                }
+                swap(minIndex, index);
+                index = minIndex;
+                leftIndex = 2*index + 1;
+            }
+        }
+
+        public void swap(int x, int y){
+            heapIndexMap.put(nodes[x], y);
+            heapIndexMap.put(nodes[y], x);
+
+            Node temp = nodes[x];
+            nodes[x] = nodes[y];
+            nodes[y] = nodes[x];
+
+        }
     }
 }
