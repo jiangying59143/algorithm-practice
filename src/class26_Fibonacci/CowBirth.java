d9cd7cbcd91e9eb29e4b06f0382dedb1c96259be
yjiang <907292671@qq.com>
2022-10-09 22:07:57 +0800

    CowBirth Fibonacci matrix quick power

--- src/class26_Fibonacci/CowBirth.java
+++ src/class26_Fibonacci/CowBirth.java
@@ -33,9 +33,63 @@ public class CowBirth {
         return ans;
     }
 
+    /**
+     * f(n) = f(n-1) + f(n-3)
+     * |f(n),f(n-1),f(n-2)| = |f(3),f(2),f(1)||A|^(n-3)
+     *
+     * 推导|A|
+     * f(n) = f(n-1) + 0*f(n-2) + f(n-3)
+     * f(n-1) = f(n-1) + 0*f(n-2) + 0*f(n-3)
+     * f(n-2) = 0*f(n-1) + f(n-2) + 0*f(n-3)
+     * =>
+     * |1,1,0|
+     * |0,0,1|
+     * |1,0,0|
+     *
+     * @param n
+     * @return
+     */
+    public int f2(int n){
+        int[][] base = new int[][]{
+                {1,1,0},
+                {0,0,1},
+                {1,0,0}
+        };
+        int[][] res = matrixPow(base, n-3);
+        return 3*res[0][0] + 2*res[1][0] + res[2][0];
+    }
+
+    private int[][] matrixPow(int[][] base, int p) {
+        int[][] ans = new int[base.length][base[0].length];
+        for (int j = 0; j < ans.length; j++) {
+            ans[j][j] = 1;
+        }
+        for(;p !=0; p >>= 1){
+            if((p&1)==1){
+                ans = matrixMultiply(ans, base);
+            }
+            base = matrixMultiply(base, base);
+        }
+        return ans;
+    }
+
+    private int[][] matrixMultiply(int[][] a, int[][] b){
+        int[][] res = new int[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                for (int k = 0; k < b.length; k++) {
+                    res[i][j] += a[i][k]*b[k][j];
+                }
+            }
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
         CowBirth cb = new CowBirth();
         System.out.println(cb.f(10));
         System.out.println(cb.f1(10));
+        System.out.println(cb.f2(10));
     }
 }
