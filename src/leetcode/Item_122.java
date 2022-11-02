b8bd34ea0a8fdc34f4b17268a32f74699e476261
yjiang <907292671@qq.com>
2022-11-02 23:09:14 +0800

    leecode 122 dp1

--- src/leetcode/Item_122.java
+++ src/leetcode/Item_122.java
@@ -2,24 +2,36 @@ package leetcode;
 
 public class Item_122 {
     public static int maxProfit(int[] prices) {
-        int sum = process(prices, 0, -1);
+        int[][] dp = new int[prices.length+1][prices.length+1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[i].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        int sum = process(prices, 0, -1, dp);
         return sum;
     }
 
-    private static int process(int[] prices, int i, int buyIndex){
+    private static int process(int[] prices, int i, int buyIndex, int[][] dp){
+        int dp_y = buyIndex+1;
+        if(dp[i][dp_y] != -1){
+            return dp[i][dp_y];
+        }
         if(i == prices.length){
+            dp[i][dp_y] = 0;
            return 0;
         }
         int sum;
         if(buyIndex == -1){
             //当前买或者不买取最大
-            sum = Math.max(-prices[i] + process(prices, i+1, i),
-                    process(prices, i+1, buyIndex));
+            sum = Math.max(-prices[i] + process(prices, i+1, i, dp),
+                    process(prices, i+1, buyIndex, dp));
         }else{
             // 当前卖或者不卖取最大
-            sum = Math.max(prices[i] + process(prices, i, -1),
-                    process(prices, i+1, buyIndex));
+            sum = Math.max(prices[i] + process(prices, i, -1, dp),
+                    process(prices, i+1, buyIndex, dp));
         }
+        dp[i][dp_y] = sum;
         return sum;
     }
 
