8324eeb078fe53c5bcb8cf81c3c29f1e4f374044
yjiang <907292671@qq.com>
2022-11-03 22:22:31 +0800

    leecode 123

--- src/leetcode/Item_123.java
+++ src/leetcode/Item_123.java
@@ -1,35 +1,19 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 public class Item_123 {
     public static int maxProfit(int[] prices) {
-        int[] res = new int[3];
+        int[][] dp = new int[3][prices.length];
+        // 第一行 为0笔交易 收益全0， 对第一个数据进行交易，只能买完卖掉， 所以收益依然是0.
+        // 第二行是 最多进行一笔交易 dp[1][1] -> dp[0][0] dp[0][1] dp[1][0] p[1]-p[0] p[1]-p[1]
+        // 第三行是 最多进行两笔交易  dp[2][5] -> dp[1][5],dp[0][5],
+        return 0;
 
-        int singTransactionSum = 0;
-        for (int i = 1; i < prices.length; i++) {
-             if(prices[i] - prices[i-1] < 0){
-                 System.out.println(singTransactionSum);
-                 if(singTransactionSum > res[0])res[0] = singTransactionSum;
-                 Arrays.sort(res);
-                 singTransactionSum = 0;
-             }else{
-                 singTransactionSum+= prices[i] - prices[i-1];
-                 if(i==prices.length-1){
-                     res[0] = singTransactionSum;
-                     Arrays.sort(res);
-                 }
-             }
-        }
-
-        return res[1] + res[2];
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{3,3,5,0,0,3,1,4};
         System.out.println(maxProfit(arr));
         arr = new int[]{1,2,4,2,5,7,2,4,9,0};
+        System.out.println(maxProfit(arr));
     }
 }
