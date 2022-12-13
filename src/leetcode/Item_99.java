907326134df99792d90536c6ff05151e848feb3f
yjiang <907292671@qq.com>
2022-12-13 18:27:48 +0800

    leecode 99

--- src/leetcode/Item_99.java
+++ src/leetcode/Item_99.java
@@ -4,32 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Item_99 {
-    static class TreeNodeObject{
-        public TreeNode preNode;
-        public TreeNode node;
-
-        public TreeNodeObject(TreeNode preNode, TreeNode node) {
-            this.preNode = preNode;
-            this.node = node;
-        }
-    }
 
     public static void recoverTree(TreeNode root) {
-        process(root, null);
+        List<TreeNode> listNodes = new ArrayList<>();
+        listNodes(root, listNodes);
+        TreeNode[] wrongNodes = new TreeNode[2];
+        for (int i = 0; i < listNodes.size()-1; i++) {
+            if(listNodes.get(i).val > listNodes.get(i+1).val){
+                if(wrongNodes[1] == null) {
+                    wrongNodes[0] = listNodes.get(i);
+                    wrongNodes[1] = listNodes.get(i + 1);
+                }else{
+                    wrongNodes[1] = listNodes.get(i + 1);
+                }
+            }
+        }
+        int temp = wrongNodes[0].val;
+        wrongNodes[0].val = wrongNodes[1].val;
+        wrongNodes[1].val = temp;
     }
 
-    public static TreeNodeObject process(TreeNode root, TreeNode pre){
+    public static void listNodes(TreeNode root, List<TreeNode> list){
         if(root == null){
-            return null;
+            return;
         }
 
-        TreeNodeObject leftObj = process(root.left, root);
-        TreeNodeObject rightObj = process(root.right, root);
-
-        return null;
+        listNodes(root.left, list);
+        list.add(root);
+        listNodes(root.right, list);
     }
 
     public static void main(String[] args) {
+        TreeNode root;
+        root = TreeNode.generate(new Integer[]{1, 3, null, null, 2});
+        recoverTree(root);
+        TreeNode.printTree(root);
 
+        root = TreeNode.generate(new Integer[]{3,1,4,null,null,2});
+        recoverTree(root);
+        TreeNode.printTree(root);
     }
 }
