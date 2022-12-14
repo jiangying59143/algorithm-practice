5ec012de53823d3031e329761998ed8808d1c657
yjiang <907292671@qq.com>
2022-12-14 18:05:53 +0800

    morris traversal

--- src/leetcode/Item_99.java
+++ src/leetcode/Item_99.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Item_99 {
@@ -28,20 +29,47 @@ public class Item_99 {
         if(root == null){
             return;
         }
-
         listNodes(root.left, list);
         list.add(root);
         listNodes(root.right, list);
     }
 
+    public static void recoverTree1(TreeNode root) {
+        TreeNode[] wrongNodes = new TreeNode[2];
+        TreeNode pre = null;
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        while(!stack.isEmpty() || root != null){
+            if(root != null){
+                stack.push(root);
+                root = root.left;
+            }else{
+                TreeNode node = stack.pop();
+                if(pre != null && pre.val > node.val){
+                    if(wrongNodes[1] != null){
+                        wrongNodes[1] = node;
+                    }else {
+                        wrongNodes[0] = pre;
+                        wrongNodes[1] = node;
+                    }
+                }
+                pre = node;
+                root = node.right;
+            }
+        }
+        int temp = wrongNodes[0].val;
+        wrongNodes[0].val = wrongNodes[1].val;
+        wrongNodes[1].val = temp;
+    }
+
     public static void main(String[] args) {
         TreeNode root;
         root = TreeNode.generate(new Integer[]{1, 3, null, null, 2});
-        recoverTree(root);
-        TreeNode.printTree(root);
+        TreeNode.print(root);
+        recoverTree1(root);
+        TreeNode.print(root);
 
         root = TreeNode.generate(new Integer[]{3,1,4,null,null,2});
-        recoverTree(root);
-        TreeNode.printTree(root);
+        recoverTree1(root);
+        TreeNode.print(root);
     }
 }
--- src/leetcode/TreeNode.java
+++ src/leetcode/TreeNode.java
@@ -1,19 +1,112 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val) { this.val = val; }
+
+    static void init(TreeNode root, List<TreeNode> mid, Map<TreeNode, Integer> map){
+        if(root == null)return;
+        midOrder(root, mid);
+        for (int i = 0; i < mid.size(); i++) {
+            map.put(mid.get(i),i);
+        }
+    }
+
+    static void midOrder(TreeNode node, List<TreeNode> mid){
+        if(node == null)return;
+        midOrder(node.left, mid);
+        mid.add(node);
+        midOrder(node.right, mid);
+    }
+
+    public static void print(TreeNode root){
+        System.out.println("Binary Tree:");
+        List<TreeNode> mid = new ArrayList<>();
+        Map<TreeNode, Integer> map = new HashMap<>();
+        init(root, mid, map);
+        List<TreeNode> nodes =  new ArrayList<>();
+        nodes.add(root);
+        treePrint(nodes, map);
+    }
+
+    public static void treePrint(List<TreeNode> nodes, Map<TreeNode, Integer> map){
+        if(nodes.isEmpty())return;
+        // nodes : 同一层节点
+        printLevel(nodes, map);//打印同一层的节点
+        List<TreeNode> children =  new ArrayList<>();
+        //顺序遍历下一层节点;
+        for (TreeNode node : nodes) {
+            if(node.left != null)children.add(node.left);
+            if(node.right != null) children.add(node.right);
+        }
+        treePrint(children, map);//递归打印下一层节点
+    }
+
+    static void printLevel(List<TreeNode> nodes, Map<TreeNode, Integer> map){
+        String VLine = "";
+        String dataLine = "";
+        String line = "";
+        int lastNodeIndex = 0;
+        int lastRightIndex = 0;
+        for (TreeNode node : nodes) {
+            int x =  map.get(node);
+            String addEmpty = getEmpty(x-lastNodeIndex);
+            lastNodeIndex = x;
+            VLine    += addEmpty+"|";//竖线拼接
+            //数字拼接
+            dataLine += addEmpty+node.val;
+            //红黑树可以用下面打印语句，打印红色；
+            //if(node.red)
+            //    dataLine+= addEmpty +"\033[91;1m"+node.data+"\033[0m";//打印红色
+            //else
+            //    dataLine += addEmpty+node.data;
+//======================组装，左右连接线=======================================
+            TreeNode left  = node.left;
+            TreeNode right = node.right;
+            String leftLine  = null;
+            String rightLine = null;
+            int leftIndex  = -1;
+            int rightIndex = -1;
+            if(left  != null){
+                leftIndex = map.get(left);
+                leftLine = getLineToSon(x - leftIndex);
+            }
+            if(right != null){
+                rightIndex = map.get(right);
+                rightLine = getLineToSon(rightIndex - x);
+            }
+            String curLine  = (leftLine == null ? "" :leftLine)  + "|"+(rightLine == null ? "" : rightLine);
+            if(leftLine == null && rightLine == null)curLine="";
+            //线段之间的间隔
+            int dif = (leftIndex == -1 ? x : leftIndex) - lastRightIndex;
+            String difEmpty = getEmpty(dif);
+            line += difEmpty + curLine;//拼接线段
+            lastRightIndex = rightIndex == -1 ? x : rightIndex;
+        }
+        System.out.println(VLine +"\n" + dataLine +"\n" + line);
+    }
+
+    static String  getEmpty(int x){
+        String empty ="";
+        for (int i = 0; i < x; i++) {
+            empty+="\t";
+        }
+        return empty;
+    }
+
+
+    //链接子线段的长度
+    static String getLineToSon(int end){
+        String line = "";
+        if(end ==0)return line;
+        for (int i = 0; i < end; i++) {
+            line+="____";
+        }
+        return line;
     }
 
     public static TreeNode generate(Integer[] arr){
