2feccd3ab4be916c62a00bb6b8a60e6d44016b1c
yjiang <907292671@qq.com>
2023-02-06 18:34:21 +0800

    rubik_cube algorithm

--- src/rubik_cube/Cube.java
+++ src/rubik_cube/Cube.java
@@ -1,41 +1,61 @@
 package rubik_cube;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Cube {
     public Item[][][] items = new Item[][][]{
-            {//FRONT
+            {//FRONT 0
                     {new Item(1, Aspect.F), new Item(2, Aspect.F), new Item(3, Aspect.F)},
                     {new Item(4, Aspect.F), new Item(5, Aspect.F), new Item(6, Aspect.F)},
                     {new Item(7, Aspect.F), new Item(8, Aspect.F), new Item(9, Aspect.F)},
             },
-            {//UP
+            {//UP 1
                     {new Item(1, Aspect.U), new Item(2, Aspect.U), new Item(3, Aspect.U)},
                     {new Item(4, Aspect.U), new Item(5, Aspect.U), new Item(6, Aspect.U)},
                     {new Item(7, Aspect.U), new Item(8, Aspect.U), new Item(9, Aspect.U)},
             },
-            {//LEFT
+            {//LEFT 2
                     {new Item(1, Aspect.L), new Item(2, Aspect.L), new Item(3, Aspect.L)},
                     {new Item(4, Aspect.L), new Item(5, Aspect.L), new Item(6, Aspect.L)},
                     {new Item(7, Aspect.L), new Item(8, Aspect.L), new Item(9, Aspect.L)},
             },
-            {//RIGHT
+            {//RIGHT 3
                     {new Item(1, Aspect.R), new Item(2, Aspect.R), new Item(3, Aspect.R)},
                     {new Item(4, Aspect.R), new Item(5, Aspect.R), new Item(6, Aspect.R)},
                     {new Item(7, Aspect.R), new Item(8, Aspect.R), new Item(9, Aspect.R)},
             },
-            {//DOWN
+            {//DOWN 4
                     {new Item(1, Aspect.D), new Item(2, Aspect.D), new Item(3, Aspect.D)},
                     {new Item(4, Aspect.D), new Item(5, Aspect.D), new Item(6, Aspect.D)},
                     {new Item(7, Aspect.D), new Item(8, Aspect.D), new Item(9, Aspect.D)},
             },
-            {//BACK
+            {//BACK 5
                     {new Item(1, Aspect.B), new Item(2, Aspect.B), new Item(3, Aspect.B)},
                     {new Item(4, Aspect.B), new Item(5, Aspect.B), new Item(6, Aspect.B)},
                     {new Item(7, Aspect.B), new Item(8, Aspect.B), new Item(9, Aspect.B)},
             }
     };
 
-    public Cube() {
+    public List[][][] dependencies = new List[][][]{
+            {
+                    {Arrays.asList(new Position(2, 0, 2), new Position(1, 2, 0)),
+                            Arrays.asList(new Position(1, 2, 1)),
+                            Arrays.asList(new Position(1, 2, 2), new Position(3, 0, 0))},
+                    {Arrays.asList(new Position(2, 1, 2)),
+                            new ArrayList(),
+                            Arrays.asList(new Position(3, 1, 0))},
+                    {Arrays.asList(new Position(2, 0, 2), new Position(4, 2, 0)),
+                            Arrays.asList(new Position(1, 2, 1)),
+                            Arrays.asList(new Position(1, 2, 2), new Position(3, 0, 0))},
+            }
+    };
 
+    public Cube() {
+        for (int i = 0; i < items.length; i++) {
+            
+        }
     }
 
     public boolean isCompleted(){
